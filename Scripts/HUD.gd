b46263b3bf9b6e9154c15e5697e561aca2aeca51
tesla <jachuwroblewski4e@gmail.com>
2025-05-16 09:01:18 +0200

    dodano ruletke

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -4,3 +4,7 @@ extends CanvasLayer
 
 func _process(_delta):
 	money_label.text = "Money: " + str(SigBank.money)
+
+func _unhandled_input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_M:
+		SigBank.money += 1000
